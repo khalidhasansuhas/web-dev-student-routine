d8e9d021143e22c1d7579a6f17ae66ec7fd2ce01
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-09-29 06:25:25 +0600

    routine info loaded

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Info from '../Info/Info';
 import Routine from '../Routine/Routine';
+import { useEffect, useState } from 'react';
 import './Main.css'
 
 const Main = () => {
+
+    const [routines,setRoutines] = useState([])
+
+    useEffect(()=>{
+        fetch('routine.json')
+        .then(res => res.json())
+        .then(data => setRoutines(data))
+        
+    },[])
+
     return (
         <div className='main-container'>
             <div className="routine-container">
-                <Routine></Routine>
+                {
+                    routines.map(routine =><Routine 
+                    key={routine.id}
+                    routine={routine}
+                    ></Routine> )
+                }
+                
             </div>
             <div className="info-container">
                 <Info></Info>
--- src/components/Routine/Routine.js
+++ src/components/Routine/Routine.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Routine.css';
 
-const Routine = () => {
+const Routine = (props) => {
+    const {id, subject, image, description, time} = props.routine;
     return (
-        <div>
-            <h1>Hi from routine container</h1>
+        <div className='routine'>
+            <img src={image} alt="" />
+            <div className="routine-info">
+                <p className='practice-topic'>Topic: {subject}</p>
+                <p><strong>Description</strong>  : <small>{description}</small></p>
+                <p><strong>Practice Time: {time}</strong> <small>minutes</small></p>
+            </div>
+            
         </div>
     );
 };
